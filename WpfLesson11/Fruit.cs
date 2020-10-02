59a258a8b3573ab580ddf16ddd8dbe7f8e35b5b9
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-02 14:15:52 +0300

    Add project files.

---
+++ WpfLesson11/Fruit.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WpfLesson11
+{
+    public class Fruit:INotifyPropertyChanged
+    {
+        private int id;
+        private string name;
+        private double price;
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+
+        public Fruit() { }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ WpfLesson11/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLesson11
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Fruit> fruits;
+        private ICollectionView view;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            fruits = new ObservableCollection<Fruit>()
+            {
+                new Fruit() { Name="Apple", Price=10 },
+                new Fruit() { Name="Apple", Price=10 },
+                new Fruit() { Name="Apple", Price=10 },
+                new Fruit() { Name="Pineapple", Price=15 },
+                new Fruit() { Name="Watermelon", Price=25 },
+                new Fruit() { Name="Grapes", Price=12 }
+            };
+            fruitsListView.ItemsSource = fruits;
+
+            view = CollectionViewSource.GetDefaultView(fruitsListView.ItemsSource);
+
+            view.SortDescriptions.Add(new SortDescription("Icon", ListSortDirection.Descending));
+            view.SortDescriptions.Add(new SortDescription("Price", ListSortDirection.Descending));
+            view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Descending));
+
+            view.GroupDescriptions.Add(new PropertyGroupDescription("Name"));
+
+            view.Filter = FilterMethod;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var tb = (sender as TextBox);
+            int i;
+            Int32.TryParse(tb.Tag.ToString(), out i);
+            fruits.FirstOrDefault(f => f.Id == i).Name = tb.Text;
+        }
+
+        private bool FilterMethod(object obj)
+        {
+            Fruit fruit = obj as Fruit;
+            if (fruit == null)
+                return true;
+            if (fruit.Name.Contains(searchTextBox.Text))
+                return true;
+            return false;
+        }
+
+        private void searchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            view.Refresh();
+        }
+
+        //private void fruitsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    ind = fruitsListView.SelectedIndex;
+        //    ICollectionView view = CollectionViewSource.GetDefaultView(fruitsListView.ItemsSource);
+        //    view.GroupDescriptions.Clear();
+        //    view.GroupDescriptions.Add(new PropertyGroupDescription("Name"));
+        //}
+    }
+}
